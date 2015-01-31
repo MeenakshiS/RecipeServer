dfd4788dcf403ed0466851cc87bef0491d81aa7b
MeenakshiS <meenakshi.saba@gmail.com>
2015-01-31 17:34:05 +0530

    Everything is ready to deploy!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RecipeServer::Application.config.secret_key_base = 'a41fa82579c9b6ff7ee34cd2a3b82744a7af873b83d6d9a1550f992bde2b2808c7914fbceec3fe88b2ad1f9b7aec8432ec2be19aedad2ec9e9ba65b7011c595e'
